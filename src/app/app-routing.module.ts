deef43c1bb2d58264befe527a8544fe763cdbdd9
Patrick ZHUANG <patrickzhuang@hotmail.fr>
2022-09-23 10:59:21 +0200

    all pages with navigation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { NewsComponent } from './news/news.component';
+import { PresentationsComponent } from './presentations/presentations.component';
+import { PromotionComponent } from './promotion/promotion.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent, pathMatch: 'full'},
+  {path: 'masters', component: PresentationsComponent},
+  {path: 'promotion', component: PromotionComponent},
+  {path: 'actualite', component: NewsComponent},
+  {path: 'contact', component: ContactComponent},
+
+  
+  {path: '**', component: HomeComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+import { PresentationsComponent } from './presentations/presentations.component';
+import { PromotionComponent } from './promotion/promotion.component';
+import { NewsComponent } from './news/news.component';
+import { ContactComponent } from './contact/contact.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    PresentationsComponent,
+    PromotionComponent,
+    NewsComponent,
+    ContactComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
